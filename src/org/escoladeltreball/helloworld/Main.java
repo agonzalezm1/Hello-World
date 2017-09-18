2a1f1c1a294dd989c8bb4ebad57601844cef5fd5
iaw47324771 <iaw47324771@h12.informatica.escoladeltreball.org>
2017-09-18 17:57:52 +0200

    primer commit

---
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw47324771
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
